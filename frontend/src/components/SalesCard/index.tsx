9991ca52ece2fbd0144ba0ed891dc8ffe3393c1e
geisonbruno1 <geisonbruno0@gmail.com>
2022-11-18 20:52:09 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,11 +19,18 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([])
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+
+        console.log(dmin);
+
+        axios.get(`${BASE_URL}sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             });
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
@@ -64,7 +71,7 @@ function SalesCard() {
                         {sales.map(sale => {
                             return (
                                 <tr key={sale.id}>
-                                    <td className="show992">{sale.date}</td>
+                                    <td className="show992">{sale.id}</td>
                                     <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
                                     <td>{sale.sellerName}</td>
                                     <td className="show992">{sale.visited}</td>
