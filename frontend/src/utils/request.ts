b1357af9dae7d1713399a73d6713282639ad112d
geisonbruno1 <geisonbruno0@gmail.com>
2022-11-19 14:43:33 -0300

    Correcao urll

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-geison.herokuapp.com/";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-geison.herokuapp.com";
